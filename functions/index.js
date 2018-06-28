67fa3be524244326591a766ef394ca20843e6c3d
trunglen <trunglenlvn@gmail.com>
2018-06-29 01:45:41 +0700

    ds

--- functions/index.js
+++ functions/index.js
@@ -15,13 +15,15 @@ app.use(cors({ origin: true }));
 app.use('/users', userApi);
 app.use('/posts', postApi);
 app.get('/website-speed', function (req, res) {
-    const website = req.param('website');
+    // const website = req.query('website');
     const start = new Date().getTime()
-    http.request(website, (response) => {
+    http.get(req.query.website, (response) => {
         const timing = new Date().getTime() - start;
-        return res.send({timing:timing});
-    })
-    return res.send({error:'unknown'});
+        console.log(response);
+        res.send({ timing: timing });
+    }).on("error", (err) => {
+        res.status(400).send({err:err.message});
+    });
 })
 app.listen(4000);
 // exports.api = functions.https.onRequest(app);
