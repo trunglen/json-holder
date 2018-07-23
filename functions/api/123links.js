7cc0d5011ef11672150b2f688261e335ee4418ae
trunglen <trunglenlvn@gmail.com>
2018-07-24 01:44:48 +0700

    ds

--- functions/api/123links.js
+++ functions/api/123links.js
@@ -1,12 +1,28 @@
 
 var express = require('express')
 var router = express.Router()
+const request = require('request')
 router.get('/token/generate', (req, res) => {
     const uid = req.param('uid', 0);
-    console.log(uid)
+    request.get('https://123link.co/api?api=555b4e5419214f7825a381e19bfa5fe0ec92e2c8&url=yourdestinationlink.com', (er, response, html) => {
+        html = html + ''
+        console.log(html.substring(0,10000))
+        console.log(html.match(/meta property="al:android:url".*refer/gi));
+        console.log(/content="fb:\/\/page\/[0-9]+/g.exec(html));
+    })
     res.send(genToken(uid))
 });
+router.get('/token/check', (req, res) => {
+    const uid = req.param('uid', 0);
+    const token = req.param('token', 0);
+    if (checkToken(uid, token)) {
+        res.send({ status: 'ok' })
+    } else {
+        res.status(400).send({ err: 'Không hợp lệ' })
+    }
+});
 const random = 'AaBbCcDdEeGgHh0123456789EeKkJjLlMmNnBbVvCcXxZz'
+let globalToken = {}
 
 function genToken(uid = '') {
     let result = ''
@@ -17,7 +33,10 @@ function genToken(uid = '') {
 }
 
 function checkToken(uid = '', token = '') {
-    let result = ''
+    if ((globalToken[uid]) === token) {
+        return false
+    }
+    globalToken[uid] = token
     for (let i = 0; i < token.length; i++) {
         if (i % 2 == 0) {
             if (token[i] !== uid[i / 2]) {
@@ -27,4 +46,5 @@ function checkToken(uid = '', token = '') {
     }
     return true
 }
+
 module.exports = router;
